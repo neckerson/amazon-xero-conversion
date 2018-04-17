f33f8ae2a13fbc81844effcff70634e734173402
Nate Eckerson <nate.eckerson@gmail.com>
2018-04-17 12:06:13 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# amazon-statement-conversion
-Prepare Amazon transaction reports for importing into Xero
+#amazon-statement-conversion.rb
+This simple Ruby script preps an Amazon transaction report for importing into
+Xero as a CSV financial statement.
+
+The first three rows of the report are removed. Dates are formatted as mm/dd/yy.
+Finally, the file is saved as a comma-delimited .csv file, vs. the default tab-delimited format.
---
+++ amazon-statement-conversion.rb
@@ -0,0 +1,24 @@
+require 'date'
+require 'csv'
+
+i = 0
+hdr = ["Date", "Order", "ID", "SKU", "Transaction type", "Payment Type", "Payment Detail", "Amount", "Quantity", "Product", "Title"]
+
+
+# Create the output file
+CSV.open('transactions.csv', 'w', col_sep: ",") do |csv|
+
+  # Open the input file (a tab-separated .txt file) and ignore the first three
+  # rows, since they aren't useful
+  CSV.open('report.txt', 'r', headers: hdr, col_sep: "\t" ).each do |row|
+    if i > 3
+
+      # The transaction report is currently formatting date as 'Jan 1, 2018'
+      # which is useless for our purposes. Using the methods below, converts
+      # the time into a date, then back into the proper format
+      row['Date'] = Date.strptime(row['Date'], '%B %d, %Y').strftime '%m/%d/%Y'
+      csv << row
+    end
+    i += 1
+  end
+end
